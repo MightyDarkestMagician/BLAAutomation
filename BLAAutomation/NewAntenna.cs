aae408ba20a12fee3140fb38ffc539edf1091f81
Alexey <brick.in.the.wall.2019@gmail.com>
2024-05-29 20:48:47 +0300

    вуав

--- BLAAutomation/NewAntenna.cs
+++ BLAAutomation/NewAntenna.cs
@@ -48,116 +48,13 @@ namespace BLAAutomation
 
         private void InitializeComponent()
         {
-            this.textBoxName = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.textBoxLength = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.textBoxAmperage = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.textBoxPower = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.textBoxFrequency = new MaterialSkin.Controls.MaterialSingleLineTextField();
-            this.buttonAddAntenna = new MaterialSkin.Controls.MaterialRaisedButton();
             this.SuspendLayout();
-            // 
-            // textBoxName
-            // 
-            this.textBoxName.Depth = 0;
-            this.textBoxName.Hint = "Name";
-            this.textBoxName.Location = new System.Drawing.Point(12, 78);
-            this.textBoxName.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxName.Name = "textBoxName";
-            this.textBoxName.PasswordChar = '\0';
-            this.textBoxName.SelectedText = "";
-            this.textBoxName.SelectionLength = 0;
-            this.textBoxName.SelectionStart = 0;
-            this.textBoxName.Size = new System.Drawing.Size(260, 23);
-            this.textBoxName.TabIndex = 0;
-            this.textBoxName.UseSystemPasswordChar = false;
-            // 
-            // textBoxLength
-            // 
-            this.textBoxLength.Depth = 0;
-            this.textBoxLength.Hint = "Length";
-            this.textBoxLength.Location = new System.Drawing.Point(12, 107);
-            this.textBoxLength.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxLength.Name = "textBoxLength";
-            this.textBoxLength.PasswordChar = '\0';
-            this.textBoxLength.SelectedText = "";
-            this.textBoxLength.SelectionLength = 0;
-            this.textBoxLength.SelectionStart = 0;
-            this.textBoxLength.Size = new System.Drawing.Size(260, 23);
-            this.textBoxLength.TabIndex = 1;
-            this.textBoxLength.UseSystemPasswordChar = false;
-            // 
-            // textBoxAmperage
-            // 
-            this.textBoxAmperage.Depth = 0;
-            this.textBoxAmperage.Hint = "Amperage";
-            this.textBoxAmperage.Location = new System.Drawing.Point(12, 136);
-            this.textBoxAmperage.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxAmperage.Name = "textBoxAmperage";
-            this.textBoxAmperage.PasswordChar = '\0';
-            this.textBoxAmperage.SelectedText = "";
-            this.textBoxAmperage.SelectionLength = 0;
-            this.textBoxAmperage.SelectionStart = 0;
-            this.textBoxAmperage.Size = new System.Drawing.Size(260, 23);
-            this.textBoxAmperage.TabIndex = 2;
-            this.textBoxAmperage.UseSystemPasswordChar = false;
-            // 
-            // textBoxPower
-            // 
-            this.textBoxPower.Depth = 0;
-            this.textBoxPower.Hint = "Power";
-            this.textBoxPower.Location = new System.Drawing.Point(12, 165);
-            this.textBoxPower.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxPower.Name = "textBoxPower";
-            this.textBoxPower.PasswordChar = '\0';
-            this.textBoxPower.SelectedText = "";
-            this.textBoxPower.SelectionLength = 0;
-            this.textBoxPower.SelectionStart = 0;
-            this.textBoxPower.Size = new System.Drawing.Size(260, 23);
-            this.textBoxPower.TabIndex = 3;
-            this.textBoxPower.UseSystemPasswordChar = false;
-            // 
-            // textBoxFrequency
-            // 
-            this.textBoxFrequency.Depth = 0;
-            this.textBoxFrequency.Hint = "Frequency";
-            this.textBoxFrequency.Location = new System.Drawing.Point(12, 194);
-            this.textBoxFrequency.MouseState = MaterialSkin.MouseState.HOVER;
-            this.textBoxFrequency.Name = "textBoxFrequency";
-            this.textBoxFrequency.PasswordChar = '\0';
-            this.textBoxFrequency.SelectedText = "";
-            this.textBoxFrequency.SelectionLength = 0;
-            this.textBoxFrequency.SelectionStart = 0;
-            this.textBoxFrequency.Size = new System.Drawing.Size(260, 23);
-            this.textBoxFrequency.TabIndex = 4;
-            this.textBoxFrequency.UseSystemPasswordChar = false;
-            // 
-            // buttonAddAntenna
-            // 
-            this.buttonAddAntenna.Depth = 0;
-            this.buttonAddAntenna.Location = new System.Drawing.Point(12, 223);
-            this.buttonAddAntenna.MouseState = MaterialSkin.MouseState.HOVER;
-            this.buttonAddAntenna.Name = "buttonAddAntenna";
-            this.buttonAddAntenna.Primary = true;
-            this.buttonAddAntenna.Size = new System.Drawing.Size(260, 36);
-            this.buttonAddAntenna.TabIndex = 5;
-            this.buttonAddAntenna.Text = "Add Antenna";
-            this.buttonAddAntenna.UseVisualStyleBackColor = true;
-            this.buttonAddAntenna.Click += new System.EventHandler(this.buttonAddAntenna_Click);
             // 
             // NewAntenna
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 271);
-            this.Controls.Add(this.buttonAddAntenna);
-            this.Controls.Add(this.textBoxFrequency);
-            this.Controls.Add(this.textBoxPower);
-            this.Controls.Add(this.textBoxAmperage);
-            this.Controls.Add(this.textBoxLength);
-            this.Controls.Add(this.textBoxName);
+            this.ClientSize = new System.Drawing.Size(300, 300);
             this.Name = "NewAntenna";
-            this.Text = "New Antenna";
-            this.Load += new System.EventHandler(this.NewAntenna_Load);
+            this.Load += new System.EventHandler(this.NewAntenna_Load_1);
             this.ResumeLayout(false);
 
         }
@@ -170,6 +67,11 @@ namespace BLAAutomation
         private MaterialSkin.Controls.MaterialRaisedButton buttonAddAntenna;
 
         private void NewAntenna_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewAntenna_Load_1(object sender, EventArgs e)
         {
 
         }
